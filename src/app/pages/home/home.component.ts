ed1e2f86ab90fec0321c19ecb10998e663051da0
victorhmartinez <victormartinezg1998@gmail.com>
2021-01-15 19:35:50 -0500

    Conexion con APIA close #2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{DataStudentService} from 'src/app/shared/services/data-student.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+lisData=[];
+  constructor( private dataStudentService:DataStudentService) {
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+    this.getListMaterias();
   }
-
+  getListMaterias(){
+    this.dataStudentService.getDataLogs('GestionProyectos').subscribe((logs:any) =>this.lisData=logs);
+    this.dataStudentService.getDataLogs('GestionProyectos').subscribe(logs =>console.log('Estas son las materias',logs));
+    // console.log('prueba')
+  }
 }
--- src/app/shared/services/data-student.service.ts
+++ src/app/shared/services/data-student.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import{ environment} from 'src/environments/environment';
+import { HttpHeaders, HttpClient } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class DataStudentService {
+private apiUrl:string;
+httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
 
-  constructor() { }
+}
+  constructor( private httpClient:HttpClient) {
+    this.apiUrl=environment.apiUrl;
+
+   }
+   public getDataLogs(subject_matter:string){
+     return this.httpClient.get(this.apiUrl+subject_matter,this.httpOptions);
+   }
 }

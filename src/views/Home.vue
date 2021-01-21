16167dcd0e7ba62ea508a7fafc82369b8eeb0641
lfbermeo <ferbermeo04@gmail.com>
2021-01-21 09:44:52 -0500

    Creada vista principal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="pa-6">
+      <h2 class="text-h2">Estudiantes</h2>
+      <!-- TABLA principal en componente -->
+      <table-students></table-students>
+    </section>
+
+    <section class="pa-6">
+      <h2 class="text-h2">Estadisticas</h2>
+      <!-- GRAFICOS GENERALES en componente -->
+      <div class="d-flex justify-space-between mb-6">
+        <circular-chart></circular-chart>
+        <curve-chart></curve-chart>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CircularChart from "../components/CircularChart.vue";
+import CurveChart from "../components/CurveChart.vue";
+import TableStudents from "../components/TableStudents.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    TableStudents,
+    CircularChart,
+    CurveChart,
+  },
 };
 </script>

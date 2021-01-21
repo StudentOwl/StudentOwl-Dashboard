1ad3a7dfa9a237140891229f49c5be31909a0f39
lfbermeo <ferbermeo04@gmail.com>
2021-01-21 09:43:41 -0500

    Added frame charts

---
+++ src/components/CurveChart.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="pa-2">
+    CircularChart
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CurveChart",
+  props: {},
+  data: () => ({}),
+};
+</script>
+
+<style>
+</style>

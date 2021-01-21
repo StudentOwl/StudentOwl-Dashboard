f6b7ba2f23362578e96ea029e1465f38f05727f0
joSar2000 <jorgitosar@gmail.com>
2021-01-21 16:36:19 -0500

    Update Detail.vue

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,12 +1,59 @@
 <template>
-  <v-container>
-    <h1>container</h1>
-  </v-container>
+  <div class="detail">
+    <section class="pa-6">
+      <!--Nombre del Estudiante-->
+      <h2 class="text-h2">Andrés Fernando Jiménez López</h2>
+      <!--Visualización de la tabla de detalles-->
+      <table-details :details="detailsData"></table-details>
+    </section>
+
+    <section class="pa-6">
+      <h2 class="text-h2">
+        Estadisticas
+      </h2>
+      <div class="d-flex justify-space-between mb-6">
+        <!--Visualización de gráfico de curvas-->
+        <curve-chart></curve-chart>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 
+  import CurveChart from "../components/CurveChart";
+  import TableDetails from "../components/TableDetails";
+
+
   export default {
-    
-  };
+    name: "Detail",
+    components: {
+      TableDetails,
+      CurveChart
+    },
+    data: () => ({
+      detailsData: [
+        {
+          aplicationName: "Google Chrome",
+          time: "5 min, 20 s",
+          date: "20/may/2020"
+        },
+        {
+          aplicationName: "Microsoft Word",
+          time: "15 min, 55 s",
+          date: "30/ene/2020"
+        },
+        {
+          aplicationName: "Enterprise",
+          time: "30 min, 00 s",
+          date: "02/abr/2020"
+        }
+      ],
+      estudents: [
+        {
+          name: "Andrés Fernando Jiménez Lopez", code: "user"
+        }
+      ]
+    })
+  }
 </script>

6c153ae265aaecba94a8ab61c706879629bba6b7
joSar2000 <jorgitosar@gmail.com>
2021-01-22 02:21:19 -0500

    Update TableDetails.vue

--- src/components/TableDetails.vue
+++ src/components/TableDetails.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
     <v-data-table
       :headers="headers"
       :items="details"
@@ -47,3 +48,47 @@
   </script>
   
   <style></style>
+=======
+  <v-data-table
+    :headers="headers"
+    :items="details"
+    :items-per-page="3"
+    class="elevation-1"
+    :loading="false"
+    loading-text="Loading... Please wait"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: "TableDetails",
+  props: {
+    details: {
+      type: Array,
+      default: () => []
+    },
+    isLoading: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data: () => ({
+    headers: [
+      {
+        text: "Aplicación",
+        align: "start",
+        sortable: true,
+        value: "applicationName"
+      },
+      { text: "Tiempo", value: "duration" },
+      { text: "Fecha", value: "time" }
+    ],
+  }),
+  computed: {
+    }
+  
+};
+</script>
+
+<style></style>
+>>>>>>> Stashed changes

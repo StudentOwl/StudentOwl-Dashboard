544a57267ef361e139debee60410d3245e8725dc
AriannaDC <ferbermeo04@gmail.com>
2021-01-22 04:35:40 -0500

    Aniadido Login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,74 @@
 <template>
-  <div>LOGIN</div>
+  <v-container class="d-flex pa-4 mt-16">
+    <v-card class="mx-auto" max-width="500">
+      <v-container class="pa-6">
+        <v-row align="center" align-content="center">
+          <v-col sm="12"
+            ><v-img
+              class="white--text align-end"
+              src="../assets/StudentOwlLogoRight.png"
+            ></v-img>
+            <h2 class="subtitle-1 text-center text--secondary">
+              Sistema de monitoreo
+            </h2>
+          </v-col>
+
+          <v-col sm="12" class="px-16 py-8">
+            <v-col sm="12"
+              ><h1 class="text-h4 text-center">Inicion de sesion</h1></v-col
+            >
+            <v-col sm="12" class="mb-n6"
+              ><v-text-field
+                label="User"
+                v-model="input.username"
+              ></v-text-field
+            ></v-col>
+
+            <v-col sm="12"
+              ><v-text-field
+                label="Password"
+                type="password"
+                v-model="input.password"
+              ></v-text-field
+            ></v-col>
+            <v-col sm="12" class="px-16">
+              <v-btn color="secondary" outlined block :click="login()">Sing In</v-btn>
+            </v-col>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Login",
+  props: {},
+  data: () => ({
+    input: {
+      username: "",
+      password: "",
+    },
+  }),
+  methods: {
+    login() {
+      if (this.input.username != "" && this.input.password != "") {
+        if (
+          this.input.username == this.$parent.mockAccount.username &&
+          this.input.password == this.$parent.mockAccount.password
+        ) {
+          this.$emit("authenticated", true);
+          this.$router.replace({ name: "Dashboard" });
+        } else {
+          console.log("El usuario y contrasenia son incorrectos");
+        }
+      } else {
+        console.log("Es necesario llenar los campos");
+      }
+    },
+  },
+};
 </script>
 
 <style></style>

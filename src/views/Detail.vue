8b8251cfd17252910eca9727da76dfd689361b8e
joSar2000 <jorgitosar@gmail.com>
2021-01-22 00:14:01 -0500

    Update Detail.vue

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -5,6 +5,8 @@
       <h2 class="text-h2">Andrés Fernando Jiménez López</h2>
       <!--Visualización de la tabla de detalles-->
       <table-details :details="detailsData"></table-details>
+     
+      <code v-if="detailsData">{{ detailsData }}</code>
     </section>
 
     <section class="pa-6">
@@ -15,6 +17,10 @@
         <!--Visualización de gráfico de curvas-->
         <curve-chart></curve-chart>
       </div>
+      <div class="d-flex justify-space-between mb-6">
+        <!--Visualización de gráfico de curvas-->
+        <chart-details :detailsCharts="detailsChart"></chart-details>
+      </div>
     </section>
   </div>
 </template>
@@ -23,37 +29,46 @@
 
   import CurveChart from "../components/CurveChart";
   import TableDetails from "../components/TableDetails";
-
+  import { getPastWeek } from "../utils/dateutils";
+  import { getLogsByComponentAndStudent } from "../utils/dataLoader";
 
   export default {
     name: "Detail",
     components: {
       TableDetails,
-      CurveChart
+      CurveChart,
+
+    },
+    props: {
+      componentId: {
+        type: String,
+        required: true
+      }
     },
     data: () => ({
-      detailsData: [
-        {
-          aplicationName: "Google Chrome",
-          time: "5 min, 20 s",
-          date: "20/may/2020"
-        },
-        {
-          aplicationName: "Microsoft Word",
-          time: "15 min, 55 s",
-          date: "30/ene/2020"
-        },
-        {
-          aplicationName: "Enterprise",
-          time: "30 min, 00 s",
-          date: "02/abr/2020"
-        }
-      ],
-      estudents: [
-        {
-          name: "Andrés Fernando Jiménez Lopez", code: "user"
-        }
+      detailsData: [],
+      pingResul: "",
+      dates: [
+        getPastWeek(new Date())
+          .toISOString()
+          .substr(0, 10),
+        new Date().toISOString().substr(0, 10)
       ]
-    })
+
+    }),
+    async created() {
+      this.loadDetailsData();
+    },
+    methods: {
+    loadDetailsData: async function() {
+      const resultado = await getLogsByComponentAndStudent(this.componentId, this.studentId, this.dates);
+      // this.pingResul = resultado.data;
+      this.detailsData = resultado.data.data;
+    },
+    
+    onDatesUpdate(newDates) {
+      this.dates = newDates;
+    }
   }
+}
 </script>

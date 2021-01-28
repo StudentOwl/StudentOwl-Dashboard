c06a608942feeb062665a230df772b0c94be0504
AriannaDC <arivinuezao@gmail.com>
2021-01-28 07:14:33 -0500

    Aniadido libreria date-fns, formateador de fechas, close #41

--- src/components/TableStudents.vue
+++ src/components/TableStudents.vue
@@ -5,11 +5,22 @@
     :items-per-page="5"
     :loading=isLoading
     item-key="username"
-    group-by="lastTime"
     class="elevation-1"
     loading-text="Cargando..."
-    show-group-by
   >
+
+    <template v-slot:item.lastDate="{ item }"
+      >
+      {{ parseDateToDate(item.lastDate) }}
+      </template
+    >
+
+    <template v-slot:item.lastTime="{ item }"
+      >
+      {{ parseDateToTime(item.lastTime) }}
+      </template
+    >
+
     <template v-slot:item.actions="{ item }"
       ><v-btn icon color="secodary" :to="`detail/${item.username}`"
         ><v-icon>mdi-information-outline</v-icon></v-btn
@@ -19,6 +30,8 @@
 </template>
 
 <script>
+import { format } from "date-fns";
+
 export default {
   name: "TableStudents",
   props: {
@@ -35,11 +48,26 @@ export default {
     headers: [
       { text: "Estudiante", value: "name" },
       { text: "Accion", value: "lastAction", sortable: false },
+      { text: "Fecha", value: "lastDate", sortable: true },
       { text: "Hora", value: "lastTime", sortable: true },
       { text: "Detalle", value: "actions", sortable: false }
     ],
   }),
   computed: {},
+  methods: {
+    parseDateToDate: function(date) {
+      if (date === '-') {
+        return '-'
+      }
+      return format(date, 'yyyy-MM-dd');
+    },
+    parseDateToTime: function(date) {
+      if (date === '-') {
+        return '-'
+      }
+      return format(date, 'HH:mm:ss');
+    }
+  }
 };
 </script>
 

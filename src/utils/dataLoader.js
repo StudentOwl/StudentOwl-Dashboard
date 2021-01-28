bee178f2537c713f2d2259f0eb7922b37bdcaf66
joSar2000 <jorgitosar@gmail.com>
2021-01-28 13:36:18 -0500

    UPDATE GET STUDENTS

--- src/utils/dataLoader.js
+++ src/utils/dataLoader.js
@@ -71,10 +71,11 @@ export async function getLogsByComponentAndStudent(
   }
 }
 
-export async function getNamesbyStudent(studentId) {
-  var student = `&msStudent=${studentId}`;
-
-  var query = `${API_URI}api/v1.0/logs/all?student=${student}`;
+export async function getNamesbyStudent(studentId, dates) {
+  var student = `&student=${studentId}`;
+  var msStart = `&msStart=${convertDateToMiliseconds(dates[0])}`;
+  var msEnd = `&msEnd=${convertDateToMiliseconds(dates[1])}`;
+  var query = `${API_URI}api/v1.0/logs/all?component=GTPR01${student}${msStart}${msEnd}`;
   console.log(query);
   try {
     const res = await axios.get(query);

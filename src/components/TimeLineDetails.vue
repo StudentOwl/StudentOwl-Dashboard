f0883f3f7e3fac6354d79da5bfd8d6aa579442d6
joSar2000 <jorgitosar@gmail.com>
2021-01-28 13:36:01 -0500

    ADD TIMELINE

--- src/components/TimeLineDetails.vue
+++ src/components/TimeLineDetails.vue
@@ -1,43 +1,48 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="details"
-    :items-per-page="3"
-    class="elevation-1"
-    :loading="false"
-    loading-text="Loading... Please wait"
-  >
-  </v-data-table>
+  <v-timeline-item color=white >
+    <template v-slot:icon>
+      <v-icon large color="teal darken-2">
+        mdi-email
+      </v-icon>
+    </template>
+    <template v-slot:opposite>
+      <span>{{timeExecution}}</span>
+    </template>
+    <v-card class="elevation-24" outlined shaped>
+      <v-card-title class="headline">
+        Aplicación Ejecutada: <br> {{ studentId.applicationName }}
+      </v-card-title>
+      <v-card-subtitle> <br>
+        Tiempo total de ejecución: {{ durationSecondstoMinutes }} minutos
+      </v-card-subtitle>
+      <v-card-text>
+        Acción realizada: {{ studentId.action }} 
+      </v-card-text>
+    </v-card>
+  </v-timeline-item>
 </template>
 
 <script>
-export default {
-  name: "TableDetails",
-  props: {
-    details: {
-      type: Array,
-      default: () => [],
+  import moment from "moment";
+  export default {
+    name: "TimeLineDetails",
+    props: {
+      studentId:
+        { default: () => [] },
     },
-    isLoading: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  data: () => ({
-    headers: [
-      {
-        text: "Aplicación",
-        align: "start",
-        sortable: true,
-        value: "applicationName",
+    computed: {
+      timeExecution() {
+        return moment(this.studentId.time).format("MM/DD/YYYY");
       },
-      { text: "Tiempo (Minutos)", value: "duration" },
-      { text: "Fecha", value: "time" },
-      { text: "Accion", value: "action" },
-    ],
-  }),
-  computed: {},
-};
+      durationSecondstoMinutes() {
+        var minute = Math.floor((this.studentId.duration / 60) % 60);
+        minute = (minute < 10) ? '0' + minute : minute;
+        var second = this.studentId.duration % 60;
+        second = (second < 10) ? '0' + second : second;
+        return minute + ':' + second;
+      },
+    },
+  };
 </script>
-};
+
 <style></style>

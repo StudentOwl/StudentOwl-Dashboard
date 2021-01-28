8a1f17bc2728f7eda3299a694a9d8bd3614bfea8
Scoowy <gahonajuanjo@gmail.com>
2021-01-28 11:37:49 -0500

    Change colors

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app color="secondary">
+    <v-navigation-drawer v-model="drawer" app color="white">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title"> Componentes </v-list-item-title>

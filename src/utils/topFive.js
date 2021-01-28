c8128b2a757eef71d0826defbdfc9d8b7ee54e2e
Scoowy <gahonajuanjo@gmail.com>
2021-01-28 16:31:56 -0500

    clean

--- src/utils/topFive.js
+++ src/utils/topFive.js
@@ -1,16 +1,13 @@
-
 import { getLogsTopFive } from "../utils/dataLoader";
-
-
 
 export async function loadLogsTopFive(componentId, dates) {
   var elementos = [];
-var repetidos = [];
-  var num = []
-  var arrName = []
+  var repetidos = [];
+  var num = [];
+  var arrName = [];
   const arrTools = [];
-  const newArr = []
-  const myObj = {}
+  const newArr = [];
+  const myObj = {};
   //Obtencion de datos de la API
   const resultado = await getLogsTopFive(componentId, dates);
   //Almacenamos en un Array los elementos guardados
@@ -19,10 +16,10 @@ var repetidos = [];
   //---------------------------------------------------------------------///
   //Obtenemos los objetos q se repiten
   repetidos = objetosDuplicados(elementos);
-  console.log("Estes el array de repetiods", repetidos)
+  // console.log("Estes el array de repetiods", repetidos);
 
   //----------------------------------------------------------------------///
- 
+
   //Obtenemos los nombres de la aplicacion
   repetidos.forEach(el => {
     newArr.push(el.applicationName);
@@ -30,43 +27,45 @@ var repetidos = [];
       // comprobamos si el valor existe en el objeto
       if (!(el in myObj)) {
         // si no existe creamos ese valor y lo añadimos al array final, y si sí existe no lo añadimos
-        myObj[el] = true
-        arrName.push(el)
+        myObj[el] = true;
+        arrName.push(el);
       }
-    })
-  })
+    });
+  });
 
   //obtenemos los tiempos
 
   num = obtenertiempos(arrName, repetidos);
   //----------------------------------------------
   //---------------------------------------//
- //Creamos nuvamente un array con los todos los herramientas y tiempo de uso
+  //Creamos nuvamente un array con los todos los herramientas y tiempo de uso
   for (let i = 0; i < arrName.length; i++) {
     var objecto = {};
-    objecto.nameTools = arrName[i]
-    objecto.duration = num[i]
-   /// console.log("Creando object", objecto)
+    objecto.nameTools = arrName[i];
+    objecto.duration = num[i];
+    /// console.log("Creando object", objecto)
     arrTools.push(objecto);
   }
   //------------------------------------//
-//Ordenamos y cortamos el arreglo para conseguir los 5 mas altos
-  let sorted = arrTools.sort(function (a, b) {
-    if (a.duration > b.duration) { return -1 };
-    if (a.duration < b.duration) { return 1 };
+  //Ordenamos y cortamos el arreglo para conseguir los 5 mas altos
+  let sorted = arrTools.sort(function(a, b) {
+    if (a.duration > b.duration) {
+      return -1;
+    }
+    if (a.duration < b.duration) {
+      return 1;
+    }
 
     return 0;
   });
   const topfiveArray = sorted.splice(0, 5);
 
-
   ///------------------------------//
-  
- 
-  console.log("nombre herramientas", arrName)
-  console.log("Total de tiempo", num)
-  console.log("Herramientas en array", arrTools)
-  console.table(topfiveArray)
+
+  // console.log("nombre herramientas", arrName);
+  // console.log("Total de tiempo", num);
+  // console.log("Herramientas en array", arrTools);
+  // console.table(topfiveArray);
   return topfiveArray;
 }
 
@@ -87,34 +86,28 @@ function objetosDuplicados(array) {
     return acc;
   }, {});
   //Filtramos todos los objetos reptidos
-  const duplicados = array.filter((array) => {
+  const duplicados = array.filter(array => {
     return busqueda[array.applicationName];
-  }
-  );
+  });
 
   return duplicados;
 }
 //Obtenemos los maximos tiempos de todas las herramientas
 function obtenertiempos(arrName, repetidos) {
-  var num = []
+  var num = [];
   let totUse;
   for (let i = 0; i < arrName.length; i++) {
     totUse = 0;
 
-
     for (let x = 0; x < repetidos.length; x++) {
-
-
       if (arrName[i] == repetidos[x].applicationName) {
         //    console.log("Probando if",repetidos[x].applicationName)
         totUse = totUse + repetidos[x].duration;
         //    console.log(repetidos[x].duration);
-
       }
     }
-    console.log("total de uso de " + arrName[i] + " uso" + totUse)
+    // console.log("total de uso de " + arrName[i] + " uso" + totUse);
     num.push(totUse);
-
   }
 
   return num;
